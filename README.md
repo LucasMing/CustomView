6113a39c85ad18cd7e6f3d2a9042a1b6653b8808
LucasMing <635382363@qq.com>
2019-10-12 10:06:38 +0800

    绘制拖动小球

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# CustomView
-自定义控件
+#  自定义控件
+###  一、实现步骤
+1. 继承View类或其子类　
+2. 重写view中的一些方法
+3. 为自定义View类增加属性（两种方式）
+4. 绘制控件（导入布局）
+5. 响应用户事件
+6. 定义回调函数（根据自己需求来选择）
+	
+### 二、需要被重写的方法
+	
+- onDraw() 具体的视图都要覆写该函数来实现自己的绘制
+- onLayout() 为viewGroup类型布局子视图用。
+- onMeasure() 用于计算视图大小（即长和宽）的方式，并通过setMeasuredDimension(width, height)保存计算结果。
+- onTouchEvent 定义触屏事件来响应用户操作。
+
+### 三.自定义控件的三种方式
+1. 继承已有的控件
+2. 继承一个布局文件
+3. 继承view
+
+### 四.自定义属性
+1. 在布局文件中直接加入属性，在构造函数中去获得
+2. 在res/values/ 下建立一个attrs.xml 来声明自定义view的属性
+
+### 五.View的绘制与更新
+View绘制分三个步骤，顺序是：onMeasure，onLayout，onDraw。调用invalidate方法只会执行onDraw方法；调用requestLayout方法只会执行onMeasure方法和onLayout方法，并不会执行onDraw方法。
+
+所以当我们进行View更新时，若仅View的显示内容发生改变且新显示内容不影响View的大小、位置，则只需调用invalidate方法；若View宽高、位置发生改变且显示内容不变，只需调用requestLayout方法；若两者均发生改变，则需调用两者，按照View的绘制流程，推荐先调用requestLayout方法再调用invalidate方法。
+
+*invalidate和postInvalidate: invalidate方法只能用于UI线程中，在非UI线程中，可直接使用postInvalidate方法。*
+    
--- app/src/main/java/com/custom/view/widget/BallView.java
+++ app/src/main/java/com/custom/view/widget/BallView.java
@@ -1,8 +1,53 @@
 package com.custom.view.widget;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.custom.view.R;
+
 /**
  * author : lcm
  * date   : 2019/10/11 17:51
  */
-public class BallView {
+public class BallView extends View {
+    private Paint paint=new Paint();;
+    public float currentX=100;
+    public float currentY=100;
+    public int textColor;
+    public float textSize;
+    public BallView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //获取资源文件里面的属性，由于这里只有一个属性值，不用遍历数组，直接通过R文件拿出color值
+        //把属性放在资源文件里，方便设置和复用
+        TypedArray array = context.obtainStyledAttributes(attrs,R.styleable.ball);
+        textColor = array.getColor(R.styleable.ball_textColor,Color.BLACK);
+        textSize = array.getDimension(R.styleable.ball_TextSize,40);
+        array.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //画一个蓝色的圆形
+        paint.setColor(Color.BLUE);
+        canvas.drawCircle(currentX,currentY,100,paint);
+        //设置文字和颜色，这里的颜色是资源文件values里面的值
+        paint.setColor(textColor);
+        paint.setTextSize(textSize);
+        canvas.drawText("拖动",currentX-50,currentY+150,paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        currentX=event.getX();
+        currentY=event.getY();
+        invalidate();//重新绘制图形
+        return true;
+    }
 }
